4774716b5fc0ec2f73ea0e488cce230a803ae5fb
jreleaserbot <jreleaser@kordamp.org>
2023-10-23 19:49:18 -0700

    clusterless v1.0-wip-68

--- Formula/clusterless.rb
+++ Formula/clusterless.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.8.0 at 2023-10-23T16:39:55.330825-07:00
+# Generated with JReleaser 1.8.0 at 2023-10-23T19:49:16.934935-07:00
 class Clusterless < Formula
   desc "Clusterless is a framework for building serverless data oriented applications."
   homepage "https://github.com/ClusterlessHQ"
   url "https://github.com/ClusterlessHQ/clusterless/releases/download/v1.0-wip-68/clusterless-1.0-wip-68.zip"
   version "1.0-wip-68"
-  sha256 "ea7dfcab9e828c155e9a44a85350c0b86a58afb7cdf2ebbef7578e8cd974f13d"
+  sha256 "98c5b4de70907e8d1f9d47b0a7125f89846cedb62e43d04939cbb7acb5a47d28"
   license "MPL-2.0"
 
   depends_on "openjdk@17"
