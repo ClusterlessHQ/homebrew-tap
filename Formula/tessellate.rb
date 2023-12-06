59aff40fefb9e5c19a079366517d1d9483ad1867
jreleaserbot <jreleaser@kordamp.org>
2023-12-06 04:44:26 +0000

    tessellate v1.0-wip-40

--- Formula/tessellate.rb
+++ Formula/tessellate.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.9.0 at 2023-12-06T04:31:58.919903Z
+# Generated with JReleaser 1.9.0 at 2023-12-06T04:44:05.203825Z
 class Tessellate < Formula
   desc "Tessellate is tool for parsing and partitioning data."
   homepage "https://github.com/ClusterlessHQ"
   url "https://github.com/ClusterlessHQ/tessellate/releases/download/v1.0-wip-40/tessellate-1.0-wip-40.zip"
   version "1.0-wip-40"
-  sha256 "9a4a83eeb3aede679fc5910d661fbe84a48dc4668169c75d4e0ec931d6760d7b"
+  sha256 "fb82af421b4bff864c45637c1a48056c715760adfd706a2a07e5f72d52a87329"
   license "MPL-2.0"
 
   depends_on "openjdk@11"
