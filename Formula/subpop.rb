6eccf5eafef48c2e7f00b4bcba647232e778dbe9
jreleaserbot <jreleaser@kordamp.org>
2024-11-05 04:42:30 +0000

    subpop v1.0-wip-10

--- Formula/subpop.rb
+++ Formula/subpop.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.15.0 at 2024-11-04T20:39:34.666491-08:00
+# Generated with JReleaser 1.15.0 at 2024-11-05T04:42:15.841310259Z
 
 class Subpop < Formula
   desc "SubPop is a command line utility for finding the differences between one or more tabular datasets."
   homepage "https://github.com/ClusterlessHQ"
-  url "https://github.com/ClusterlessHQ/subpop/releases/download/v1.0-wip-dev/subpop-1.0-wip-dev.zip"
-  version "1.0-wip-dev"
-  sha256 "4a06451ba45a5420863f1ceb1a1f00c1a2dc69e8c169bcb58405211030ea71f3"
+  url "https://github.com/ClusterlessHQ/subpop/releases/download/v1.0-wip-10/subpop-1.0-wip-10.zip"
+  version "1.0-wip-10"
+  sha256 "a6d94c52c16e7ea87fc5f7e52b2213de271b454b9f9b3614204fb874d6517bdb"
   license "MPL-2.0"
 
   depends_on "openjdk@21"
@@ -17,6 +17,6 @@ class Subpop < Formula
 
   test do
     output = shell_output("#{bin}/subpop --version")
-    assert_match "1.0-wip-dev", output
+    assert_match "1.0-wip-10", output
   end
 end
