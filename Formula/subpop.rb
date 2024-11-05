bc7af90072314265085de286c2793da83cd3a913
jreleaserbot <jreleaser@kordamp.org>
2024-11-04 20:39:48 -0800

    subpop v1.0-wip-dev

--- Formula/subpop.rb
+++ Formula/subpop.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.15.0 at 2024-11-04T16:30:29.629605119Z
+# Generated with JReleaser 1.15.0 at 2024-11-04T20:39:34.666491-08:00
 
 class Subpop < Formula
   desc "SubPop is a command line utility for finding the differences between one or more tabular datasets."
   homepage "https://github.com/ClusterlessHQ"
-  url "https://github.com/ClusterlessHQ/subpop/releases/download/v1.0-wip-9/subpop-1.0-wip-9.zip"
-  version "1.0-wip-9"
-  sha256 "0e39688dd958ff52bb40edd58b66c6264a565bdce039ae1b72a3b22e84fb2dae"
+  url "https://github.com/ClusterlessHQ/subpop/releases/download/v1.0-wip-dev/subpop-1.0-wip-dev.zip"
+  version "1.0-wip-dev"
+  sha256 "4a06451ba45a5420863f1ceb1a1f00c1a2dc69e8c169bcb58405211030ea71f3"
   license "MPL-2.0"
 
   depends_on "openjdk@21"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/subpop" => "subpop"
+    (bin/"subpop").write_env_script libexec/"bin/subpop", JAVA_HOME: Formula["openjdk@21"].opt_prefix
   end
 
   test do
     output = shell_output("#{bin}/subpop --version")
-    assert_match "1.0-wip-9", output
+    assert_match "1.0-wip-dev", output
   end
 end
