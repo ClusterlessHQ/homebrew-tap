91475ab83f41334402772329a60251d1d8ad520d
jreleaserbot <jreleaser@kordamp.org>
2024-11-04 21:37:49 -0800

    subpop v1.0-wip-dev

--- Formula/subpop.rb
+++ Formula/subpop.rb
@@ -1,15 +1,15 @@
-# Generated with JReleaser 1.15.0 at 2024-11-04T21:33:46.036741-08:00
+# Generated with JReleaser 1.15.0 at 2024-11-04T21:37:42.273755-08:00
 
 class Subpop < Formula
   desc "SubPop is a command line utility for finding the differences between one or more tabular datasets."
   homepage "https://github.com/ClusterlessHQ"
   url "https://github.com/ClusterlessHQ/subpop/releases/download/v1.0-wip-dev/subpop-1.0-wip-dev.zip"
   version "1.0-wip-dev"
-  sha256 "166e7f2b7884b526ed824e737382ea7a0013c86706f81f1f4787a364e8a20a51"
+  sha256 "da222430e8e7fb392fc94eba972b3a4e06a097faa76bd2fd9ae7dee2b9a1a087"
   license "MPL-2.0"
 
-  depends_on "jdk" => "graalvm-jdk@21"
-  depends_on "jdk" => "graalvm-jdk@21"
+  depends_on "graalvm-jdk@21"
+  depends_on "graalvm-jdk@21"
 
   def install
     libexec.install Dir["*"]
