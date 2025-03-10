e2f76c2e2109faaa76b1097c700c3569dc587f39
jreleaserbot <jreleaser@kordamp.org>
2025-03-10 21:50:44 +0000

    clusterless v1.0-wip-86

--- Formula/clusterless.rb
+++ Formula/clusterless.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.8.0 at 2025-01-31T19:55:43.766129874Z
+# Generated with JReleaser 1.8.0 at 2025-03-10T21:50:26.964692047Z
 class Clusterless < Formula
   desc "Clusterless is a framework for building serverless data oriented applications."
   homepage "https://github.com/ClusterlessHQ"
-  url "https://github.com/ClusterlessHQ/clusterless/releases/download/v1.0-wip-85/clusterless-1.0-wip-85.zip"
-  version "1.0-wip-85"
-  sha256 "8c29af87be27956038510730e7e3386cbecb3ae565cf40848132d5f9a482935f"
+  url "https://github.com/ClusterlessHQ/clusterless/releases/download/v1.0-wip-86/clusterless-1.0-wip-86.zip"
+  version "1.0-wip-86"
+  sha256 "5e29c9e19760fcaaaaf070a5b7f38122e6cc884a092533eae2c683caac91685a"
   license "MPL-2.0"
 
   depends_on "openjdk@17"
@@ -16,6 +16,6 @@ class Clusterless < Formula
 
   test do
     output = shell_output("#{bin}/cls --version")
-    assert_match "1.0-wip-85", output
+    assert_match "1.0-wip-86", output
   end
 end
